a9561e83b74810eaa74e2b1cbeba678f1866078d
schopenhauerzhang <schopenhauerzhang@gmail.com>
2019-06-12 23:56:26 +0800

    v7

--- main.py
+++ main.py
@@ -4,9 +4,7 @@
 # @Author:Schopenhauerzhang@icloud.com(Schopenhauerzhang@gmail.com)
 # @license:Copyright Schopenhauerzhang@icloud.com All rights Reserved.  
 # @Time: 2019-05-04 19:03
-# v2
-# v4
-# v5
-# v6
+# v7
+
 if __name__ == 'main':
 	print('SMTPMail Application')
