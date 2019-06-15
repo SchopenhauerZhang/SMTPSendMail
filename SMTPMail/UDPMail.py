815824c2f38b7a00841d91b012ad55a1782dca3c
schopenhauerzhang <schopenhauerzhang@gmail.com>
2019-06-15 16:41:44 +0800

    add udp mail

---
+++ SMTPMail/UDPMail.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# @File: UDPMail.py
+# --coding:utf-8--
+# @Author:Schopenhauerzhang@icloud.com(Schopenhauerzhang@gmail.com)
+# @license:Copyright Schopenhauerzhang@icloud.com All rights Reserved.  
+# @Time: 2019-06-15 16:41
