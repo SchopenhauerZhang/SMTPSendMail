0f344ccffe0eb98e19cd6621da9428c42ab7f596
schopenhauerzhang <schopenhauerzhang@gmail.com>
2019-06-15 16:09:17 +0800

    ftp mail

---
+++ SMTPMail/FTPMail.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# @File: FTPMail.py
+# --coding:utf-8--
+# @Author:Schopenhauerzhang@icloud.com(Schopenhauerzhang@gmail.com)
+# @license:Copyright Schopenhauerzhang@icloud.com All rights Reserved.  
+# @Time: 2019-06-15 16:09
